25842c5906e6a359a487c99c2c8ea42b548e6f7f
SamRoehrich <sam_roehrich@icloud.com>
2022-04-14 14:21:02 -0700

    added ts-jest

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,3 @@
-'use strict';
-const __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : {default: mod};
-  };
-Object.defineProperty(exports, '__esModule', {value: true});
-const twClassnames_1 = __importDefault(require('./src/util/twClassnames'));
-exports.default = twClassnames_1.default;
+import twClassnames from './src/util/twClassnames';
+export default twClassnames;
 //# sourceMappingURL=index.js.map
--- dist/src/util/twClassnames.js
+++ dist/src/util/twClassnames.js
@@ -1,18 +1,15 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', {value: true});
-function twClassnames(preset, custom) {
-  const classes = [...custom.split(' '), ...preset.split(' ')];
-  for (let i = 0; i < classes.length; i++) {
-    if (classes[i].includes('-')) {
-      const regex = new RegExp(classes[i].split('-')[0] + '-');
-      classes.forEach((item, idx) => {
-        if (idx !== i && item.match(regex)) {
-          classes.splice(idx);
+export default function twClassnames(preset, custom) {
+    const classes = [...custom.split(' '), ...preset.split(' ')];
+    for (let i = 0; i < classes.length; i++) {
+        if (classes[i].includes('-')) {
+            const regex = new RegExp(classes[i].split('-')[0] + '-');
+            classes.forEach((item, idx) => {
+                if (idx !== i && item.match(regex)) {
+                    classes.splice(idx);
+                }
+            });
         }
-      });
     }
-  }
-  return classes.join(' ');
+    return classes.join(' ');
 }
-exports.default = twClassnames;
 //# sourceMappingURL=twClassnames.js.map
